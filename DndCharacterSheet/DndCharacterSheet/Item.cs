be23d21290c8bafe33d4cd428625de0d5eedf6be
Mr-Boberino <89468586+Mr-Boberino@users.noreply.github.com>
2023-01-21 20:17:07 -0600

    Added Constructors and Updated Methods
    Added specified stat mod methods within Stats.cs and added constructors to Stats.cs and Item.cs

---
+++ DndCharacterSheet/DndCharacterSheet/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndCharacterSheet
+{
+    public class Item
+    {
+        private string name;
+        private string description;
+        private int cost;
+
+        public Item(string itemName, string itemDescription, int itemCost) {
+            name = itemName;
+            description = itemDescription;
+            cost = itemCost;
+        }
+    }
+}
--- DndCharacterSheet/DndCharacterSheet/Stats.cs
+++ DndCharacterSheet/DndCharacterSheet/Stats.cs
@@ -1,84 +1,51 @@
-﻿using System;
+﻿namespace DndCharacterSheet {
+    public class Stats
+    {
+        private SByte strength { get; set; }
+        private SByte dexterity { get; set; }
+        private SByte constitution { get; set; }
+        private SByte intelligence { get; set; }
+        private SByte wisdom { get; set; }
+        private SByte charisma { get; set; }
 
-public class Stats
-{
-	private byte strength { get; set; }
-	private byte dexterity { get; set; }
-	private byte constitution { get; set; }
-	private byte wisdom { get; set; }
-	private byte intelligence { get; set; }
-	private byte charisma { get; set; }
+        public Stats(SByte STR, SByte DEX, SByte CON, SByte INT, SByte WIS, SByte CHA)
+        {
+            strength = STR;
+            dexterity = DEX;
+            constitution = CON;
+            intelligence = INT;
+            wisdom = WIS;
+            charisma = CHA;
+        }
 
-	public sbyte getMod(int stat)
-	{
-		/*if (stat <= 1)
-		{
-			return -5;
-		}
-		else if (stat <= 3)
-		{
-			return -4;
-		}
-		else if (stat <= 5)
-		{
-			return -3;
-		}
-		else if (stat <= 7)
-		{
-			return -2;
-		}
-		else if (stat <= 9)
-		{
-			return -1;
-		}
-		else if (stat <= 11)
-		{
-			return 0;
-		}
-		else if (stat <= 13)
-		{
-			return 1;
-		}
-		else if (stat <= 15)
-		{
-			return 2;
-		}
-		else if (stat <= 17)
-		{
-			return 3;
-		}
-		else if (stat <= 19)
-		{
-			return 4;
-		}
-		else if (stat <= 21)
-		{
-			return 5;
-		}
-		else if (stat <= 23)
-		{
-			return 6;
-		}
-		else if (stat <= 25)
-		{
-			return 7;
-		}
-		else if (stat <= 27)
-		{
-			return 8;
-		}
-		else if (stat <= 29)
-		{
-			return 9;
-		}
-		else if (stat <= 30)
-		{
-			return 10;
-		}
-		else
-		{
-			return 11;
-		}*/
-		return Convert.ToSByte((stat - stat % 2 - 10)/2);
-	}
+        public sbyte getStrMod()
+        {
+            return Convert.ToSByte((strength - (strength % 2) - 10) / 2);
+        }
+
+        public sbyte getDexMod()
+        {
+            return Convert.ToSByte((dexterity - (dexterity % 2) - 10) / 2);
+        }
+
+        public sbyte getConMod()
+        {
+            return Convert.ToSByte((constitution - (constitution % 2) - 10) / 2);
+        }
+
+        public sbyte getIntMod()
+        {
+            return Convert.ToSByte((intelligence - (intelligence % 2) - 10) / 2);
+        }
+
+        public sbyte getWisMod()
+        {
+            return Convert.ToSByte((wisdom - (wisdom % 2) - 10) / 2);
+        }
+
+        public sbyte getChaMod()
+        {
+            return Convert.ToSByte((charisma - (charisma % 2) - 10) / 2);
+        }
+    }
 }
