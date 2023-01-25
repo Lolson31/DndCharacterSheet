5b18a8b7522bd5a6f54d4ecce496e1a49d1cc582
Mr-Boberino <ltrimpey2@huskers.unl.edu>
2023-01-25 12:34:13 -0600

    Create CharacterSettings.cs
    Created CharacterSettings with bool values and corresponding setter

---
+++ DndCharacterSheet/DndCharacterSheet/CharacterSettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndCharacterSheet
+{
+    internal class CharacterSettings
+    {
+        private bool xpLeveling;
+        private bool rolledHP;
+        private bool useFeats;
+        private bool mcRequirements;
+
+        public CharacterSettings()
+        {
+            xpLeveling= xp;
+            rolledHP= rollHP;
+            useFeats= feats;
+            mcRequirements= multiclass;
+        }
+
+        public setXP(bool OnOff) 
+        { 
+            xpLeveling= OnOff;
+        }
+
+        public setRolledHP(bool OnOff) 
+        { 
+            rolledHP= OnOff;
+        }
+
+        public setUseFeats(bool OnOff)
+        {
+            useFeats = OnOff;
+        }
+
+        public setMCRequirments(bool OnOff)
+        {
+            mcRequirements = OnOff;
+        }
+    }
+}
