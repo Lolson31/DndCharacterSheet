91fc699f8c613af8b44f75debd04c20c65fcd27c
Luke Olson <lolson31@huskers.unl.edu>
2023-01-30 15:03:12 -0600

    Created feature class, added elements
    added a description, level, source, and ASIs to feature class

---
+++ DndCharacterSheet/DndCharacterSheet/Feature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// A feature to be used by characters or races
+/// </summary>
+public class Feature
+{
+	private string description;
+	private sbyte level;
+	private string source;
+	public enum ASIs { 
+		Str = 0,
+		Dex = 0,
+		Con = 0,
+		Int = 0,
+		Wis = 0,
+		Cha = 0
+	}
+
+	public Feature()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
