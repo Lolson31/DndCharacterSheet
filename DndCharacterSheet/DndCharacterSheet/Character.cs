e351dd86d72e5d40b7ce189d4b9efb3da0d02d8c
Luke Olson <lolson31@huskers.unl.edu>
2023-01-30 14:28:02 -0600

    Created Character Class, added elements
    Added levelTotal, xp, hp, hpRolls, speed, size, age, languages, and classes

---
+++ DndCharacterSheet/DndCharacterSheet/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Contains level, stats, xp, hp rolls, hp, speed, size, age, language, and class(es)
+/// </summary>
+public class Character
+{
+	private sbyte levelTotal;
+	// TODO: Add race once the race class is created
+	private sbyte[] xp;
+	private sbyte[] hpRolls;
+	private short hp;
+	private sbyte speed;
+	private enum Size { };
+	private short age;
+	private string[] languages;
+	private Dictionary<string, sbyte> classes;
+
+	public Character()
+	{
+		// TODO: constructor
+	}
+}
